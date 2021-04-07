6fa451f59bdd170f8701511d55b8450bf50a56d4
easella <74786054+easella@users.noreply.github.com>
2021-04-07 02:46:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,8 @@ app.use('/', express.static(__dirname + '/public'));
 // this is for users who's form actually submitted due to JS being disabled or whatever
 app.get("/no-js", function(req, res) {
     // grab the "url" parameter from the querystring
-    var site = querystring.parse(url.parse(req.url).query).url;
+    varo=btoa(url)
+    var site = querystring.parse(url.parse(req.url).query).o
     // and redirect the user to /proxy/url
     res.redirect(unblockerConfig.prefix + site);
 });
